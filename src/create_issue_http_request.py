e76108d19a6303df884613b1a624e69c0f0f41ea
Your Name <ha.nguyen@dzsi.com>
2021-06-17 17:18:40 +0700

    Add more milestone when create issue

--- src/create_issue_http_request.py
+++ src/create_issue_http_request.py
@@ -4,10 +4,10 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-def create_issue(project="NOSVG", summary=None, description=None, issue_type=None, fix_version=None, assignee_name=None):
+def create_issue(project="NOSVG", summary=None, description=None, issue_type=None, fix_version=None, assignee_name=None, milestone=None):
   url = "https://jira.dzsi.com/rest/api/2/issue"
   username = "ha.nguyen"
-  password = ""
+  password = "matkhau1"
   auth = HTTPBasicAuth(username, password)
   headers = {
    "Accept": "application/json",
@@ -21,6 +21,7 @@ def create_issue(project="NOSVG", summary=None, description=None, issue_type=Non
   issue_dict['issuetype'] = {'name': issue_type}
   issue_dict["fixVersions"] = [{"name": fix_version}]
   issue_dict['assignee'] = {'name': assignee_name} 
+  issue_dict['customfield_10842'] = milestone
   fields["fields"] = issue_dict
   payload = json.dumps(fields)# convert object python to json string
   print(type(payload))
@@ -31,12 +32,16 @@ def create_issue(project="NOSVG", summary=None, description=None, issue_type=Non
      headers=headers,
      auth=auth
   )
-
   print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
   return  
+# list all of issues by status
+#def get_issue_list(project=None, ):
+ 
 
 if __name__ == '__main__':
-  create_issue(summary='Handt_testing', fix_version='Handt_python', description="Ha test", issue_type="Bug", assignee_name="ha.nguyen")
+  create_issue(summary='Handt_testing', fix_version='Handt_python', description="Ha test", issue_type="Bug", assignee_name="ha.nguyen", milestone = "Handt_python")
+
+
 #Milestone
 #customfield_10842
 
